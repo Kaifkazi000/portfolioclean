2bd4ecc4a950dc76c02af6cdd53db31a6db7ce1c
Kaifkazi000 <kaifoddinkazi@gmail.com>
2026-01-02 16:29:47 +0530

    add crt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const navItems = [
   { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
   { name: "Projects", href: "#projects" },
-  { name: "Certifications", href: "#certifications" },
+  { name: "Certs", href: "#certifications" },
   { name: "Contact", href: "#contact" },
 ]
 
@@ -116,7 +116,7 @@ export default function Navbar({ setIsSideMenuOpen }: NavbarProps) {
               className="p-2 sm:p-3 bg-white/10 hover:bg-black/20 rounded-full"
               aria-label="Scroll to Certifications"
             >
-              <h6 className="text-sm sm:text-xl font-semibold text-white">Certifications</h6>
+              <h6 className="text-sm sm:text-xl font-semibold text-white">Certs</h6>
             </button>
             <button
               onClick={() => scrollToSection("#contact")}
